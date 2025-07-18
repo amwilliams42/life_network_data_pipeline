dae30950c826659f5d6cd66a6eab0d5b766a9153
Alex Williams <92amwilliams@gmail.com>
2025-07-18 11:39:42 -0500

    add comprehensive requirements.txt for dependency management

--- flows/dbt.py
+++ flows/dbt.py
@@ -1,15 +1,22 @@
 from prefect import flow
 from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
+from prefect_dbt.cli.commands import trigger_dbt_cli_command
 
 
 @flow
 def run_dbt():
+
+    trigger_dbt_cli_command(
+        command="dbt deps", project_dir="./lan_dbt/",
+    )
+
     PrefectDbtRunner(
         settings=PrefectDbtSettings(
             project_dir="lan_dbt",
             profiles_dir="lan_dbt"
         )
     ).invoke(["build"])
+
 
 
 if __name__ == "__main__":
