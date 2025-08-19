f197293819673a49807eabfce5f7ac66db2bb0b2
Alex Williams <92amwilliams@gmail.com>
2025-08-19 14:08:04 -0500

    adding NATS, testing NATS, requirements.txt

--- flows/messaging.py
+++ flows/messaging.py
@@ -7,12 +7,13 @@ import nats
 
 @task
 async def send_payload(subject, payload):
-    async def _inner():
-        nc = await nats.connect("nats://natffs:4222")
+        nc = await nats.connect("nats://nats:4222")
         await nc.publish(subject, payload)
         await nc.flush()
 
 
 @flow
 async def run_test():
+    logger = logging.get_run_logger()
     await send_payload("test", b'test message from prefect')
+    logger.info("Sent test message")
